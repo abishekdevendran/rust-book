443ea6bd9f3a92b03c957024a6fbcd4a21a41a8a
Abishek Devendran <abishekdevendran@gmail.com>
2025-03-31 14:28:57 +0530

    Ownership

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,8 +7,18 @@ fn main() {
     for i in (3..=10).rev() {
         println!("{i}");
     }
+    println!("First word: {}", first_word("Hello world"));
 }
 
 fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+
+fn first_word(s: &str) -> &str {
+    for (i, &el) in s.as_bytes().iter().enumerate() {
+        if el == b' ' {
+            return &s[0..i];
+        }
+    }
+    s
 }
