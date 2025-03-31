79e080b79927c620717d4ada8e87c133f0637ef1
Abishek Devendran <abishekdevendran@gmail.com>
2025-03-31 13:56:24 +0530

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    println!("The sum of 2 and 3 is {}", add(2, 3));
+    for i in (3..=10).rev() {
+        println!("{i}");
+    }
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
