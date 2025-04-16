c9ec92cd868a9af82effc6cff94e8c0977eadcb5
D Abishek <abishek.d@randomwalk.ai>
2025-04-16 11:41:40 +0530

    Added struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,15 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+impl IpAddrKind {
+    fn new (v: IpAddrKind) -> IpAddrKind {
+        v
+    }
+}
 
 fn main() {
     println!("Hello, world!");
+    let home = IpAddrKind::new(IpAddrKind::V4(127, 0, 0, 1));
 }
